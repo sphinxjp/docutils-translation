d64e92f05136b04a31b06d50dfd9869c0a5572cb
shimizukawa <shimizukawa@gmail.com>
2014-01-26 14:42:41 +0900

    add droneio.sh script

---
+++ droneio.sh
@@ -0,0 +1,31 @@
+#hg clone https://bitbucket.org/sphinxjp/docutils-translation
+# cwd is cloned from https://bitbucket.org/sphinxjp/docutils-translation
+
+##################################
+# get docutils source
+svn export --force http://svn.code.sf.net/p/docutils/code/trunk .
+
+################################
+# setup sphinx and others
+pip install sphinx boto_rsync babel sphinx-intl transifex-client
+
+################################
+# setup transifex setting files
+sphinx-intl create-transifexrc        #create ~/.transifexrc
+
+
+#######################################
+# make pot files and push to transifex
+sphinx-build -b gettext -d _build/doctrees . locales/pot
+sphinx-intl --transifex-project-name=docutils update-txconfig-resources
+tx push -s
+
+###########################
+# make translated document
+tx pull --all                         #pull po files from transifex
+sphinx-intl build                     #compile po -> mo
+sphinx-build -b html -d _build/doctrees -Dlanguage=ja . _build/html
+
+##################################
+# deploy to s3
+boto-rsync --delete -g public-read _build/html s3://docutils.sphinx-users.jp/
