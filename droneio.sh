c275052f9e80aa6d1c407e28e7582f93a1ef8874
shimizukawa <takayuki.shimizukawa@beproud.jp>
2014-10-29 13:38:28 +0900

    use wheelhouse to setup environment

--- droneio.sh
+++ droneio.sh
@@ -1,6 +1,5 @@
 # you need setup Environment Variables as:
 #
-#  PIP_USE_WHEEL=true
 #  SPHINXINTL_TRANSIFEX_USERNAME=<YOUR-TRANSIFEX-ID>
 #  SPHINXINTL_TRANSIFEX_PASSWORD=<YOUR-TRANSIFEX-PW>
 #  SPHINXINTL_LOCALE_DIRS=locale
@@ -13,10 +12,26 @@
 # get docutils source
 svn export --force http://svn.code.sf.net/p/docutils/code/trunk .
 
+##################################
+# get wheel packages
+curl -L -s https://www.dropbox.com/s/pmxl0g8ghwmrgj0/sphinx-wheelhouse-20140503.tgz | tar vzxf -
+curl -L -s https://www.dropbox.com/s/cmy7e6frsv1mf6a/sphinx-intl-wheelhouse-20140503.tgz | tar vzxf -
+
+export PIP_FIND_LINKS=wheelhouse
+python wheelhouse/get-pip.py
+
+
 ################################
 # setup sphinx and others
 pip install --pre sphinx
-pip install sphinx-intl transifex-client
+
+pip install sphinx-intl transifex-client==0.8
+## transifex-client 0.9 has a SSL issue. 0.10 is not fine to upload new resource.
+
+### WHY??
+ls -laFd /home/ubuntu/virtualenv/python2.7/bin/tx
+chmod +x /home/ubuntu/virtualenv/python2.7/bin/tx
+
 
 ################################
 # setup transifex setting files
